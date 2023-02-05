4eb9283d0aa3ee63660fc866897a00105c1d0da7
Haoran Yin <h.yin@umail.leidenuniv.nl>
2023-02-05 22:27:03 +0100

    rename shell script

---
+++ atom_experiment.sh
@@ -0,0 +1,16 @@
+description=$1
+folder_path=$2
+features="disp ela_distr ela_level ela_meta ic limo nbc pca"
+
+if [ ! -d "$description" ]; then
+    mkdir $description
+fi
+
+for id in {1..5}
+do
+    for feature in $features
+    do
+        python scripts/atom_process.py -d=$description -p=$folder_path -f=$feature -i=$id &
+        echo $id $feature
+    done
+done
