0e11059659e9094f0e5014a859bcbce79b7e1fe7
Haoran Yin <h.yin@umail.leidenuniv.nl>
2023-02-05 23:03:02 +0100

    remove

--- format_filename.py
+++ format_filename.py
@@ -1,9 +1,23 @@
 import os
 import re
+import pandas as pd
+import numpy as np
 
-os.chdir("/data/s3202844/data/subtract")
-file_list = os.listdir("/data/s3202844/data/subtract")
-print(file_list)
+# os.chdir("/data/s3202844/data/subtract")
+# file_list = os.listdir("/data/s3202844/data/subtract")
+# print(file_list)
+# df = pd.read_csv("/data/s3202844/data/experiment_subtract.csv")
+# header = df.columns.values[1:].tolist()
+# header = ["problem_id", "experiment_id", "subtract_lim", "rotate_lim",
+#           "scale_factor", "is_subtract", "is_rotate", "is_scale"] + header[4:]
+# data = df.values[:, 1:]
+# dataset = []
+# for i in range(data.shape[0]):
+#     dataset += [[data[i][0], data[i][1], data[i][2], 0.0,
+#                  1.0, data[i][3], 0, 0] + data[i][4:].tolist()]
+# dataset = np.array(dataset)
+# dataset = pd.DataFrame(dataset, columns=header)
+# dataset.to_csv("/data/s3202844/data/experiment_subtract.csv", index=False)
 # for file_name in file_list:
 #     match_obj = re.match(
 #         r"(\d+)_(\d+)_(\d+\.\d+)_(\d+\.\d+)_(\d+)_(\d+)\.txt",
