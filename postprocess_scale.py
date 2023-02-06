61db8704903a951484fe249b49d3e453400978f8
Haoran Yin <h.yin@umail.leidenuniv.nl>
2023-02-06 23:29:17 +0100

    postprocess scale

--- postprocess_scale.py
+++ postprocess_scale.py
@@ -1,47 +1,33 @@
+import os
 import math
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from scipy.stats import ks_2samp
 
-df = pd.read_csv("results/experiment_scale.csv")
+def string_to_list(string):
+    string = string[1:-1]
+    content = string.split(", ")
+    return [float(c) for c in content]
+
+
 factors = [0.015625, 0.03125, 0.0625, 0.125, 0.25, 0.5,
            1., 2., 4., 8., 16., 32., 64., 128.]
-
-header = df.columns.values.tolist()
-feature_list = header[8:]
-basic_distr = [[] for _ in range(5)]
-for problem_id in range(1, 6):
-    df_new = df[(df["problem_id"] == problem_id) & (df["is_scale"] == 0)]
-    for c in feature_list:
-        basic_distr[problem_id-1] += [pd.to_numeric(df_new[c].tolist())]
-basic_distr = np.array(basic_distr)
-
-dataset = []
-for problem_id in range(1, 6):
-    for scale_factor in factors:
-        df_new = df[(df["problem_id"] == problem_id) &
-                    (df["scale_factor"] == scale_factor) &
-                    (df["is_scale"] == 1)]
-        rec = []
-        for c in feature_list:
-            p = pd.to_numeric(df_new[c].tolist())
-            q = basic_distr[problem_id-1][len(rec)]
-            statistic, pvalue = ks_2samp(p, q)
-            rec += [[statistic, pvalue]]
-        dataset += [[problem_id, 0, 0.0, 0.0, scale_factor, 0, 0, 1] + rec]
-
-dataset_df = pd.DataFrame(dataset, columns=header)
-dataset_df.to_csv("results/kstest_scale.csv")
-
+os.chdir("/data/s3202844/data")
+df = pd.read_csv("experiment_scale_kstest.csv")
+os.chdir("/home/s3202844/results")
+dataset_list = df.values.tolist()
+columns = df.columns.values.tolist()
+feature_list = columns[8:]
 x = [math.log(f, 2) for f in factors]
 for problem_id in range(1, 6):
     plt.figure(figsize=(15, 20))
     for i in range(len(feature_list)):
-        pvalue = np.array(
-            dataset_df[(dataset_df["problem_id"] == problem_id)][
-                feature_list[i]].tolist())[:, 1]
+        pvalue = []
+        for f in factors:
+            ks_string = df[(df["problem_id"] == float(problem_id)) & 
+                (df["scale_factor"] == float(f))][feature_list[i]].tolist()[0]
+            ks_list = string_to_list(ks_string)
+            pvalue += [ks_list[1]]
         plt.subplot(9, 6, i+1)
         plt.ylim(-0.1, 1.1)
         plt.plot(x, pvalue)
@@ -49,5 +35,5 @@ for problem_id in range(1, 6):
         plt.axhline(0.05, color="red", linestyle=":")
         plt.title(feature_list[i])
     plt.tight_layout()
-    plt.savefig("results/pvalue_scale_" + str(problem_id) + ".png")
+    plt.savefig("pvalue_scale_" + str(problem_id) + ".png")
     plt.cla()
