1185819d734664134099c05c277adf2728f8ffd9
Haoran Yin <h.yin@umail.leidenuniv.nl>
2023-02-13 00:34:52 +0100

    postprocess rotate

--- postprocess_rotate.py
+++ postprocess_rotate.py
@@ -1,7 +1,11 @@
 import os
 import math
+import joypy
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+from scipy.stats import ks_2samp
 
 
 def string_to_list(string):
@@ -10,30 +14,36 @@ def string_to_list(string):
     return [float(c) for c in content]
 
 
-factors = [0.015625, 0.03125, 0.0625, 0.125, 0.25, 0.5,
-           1., 2., 4., 8., 16., 32., 64., 128.]
 os.chdir("/data/s3202844/data")
-df = pd.read_csv("experiment_scale_kstest.csv")
-os.chdir("/home/s3202844/results")
+df = pd.read_csv("experiment_rotation_distr.csv")
+if not os.path.exists("/home/s3202844/results/experiment_rotation/"):
+    os.mkdir("/home/s3202844/results/experiment_rotation/")
+os.chdir("/home/s3202844/results/experiment_rotation/")
 dataset_list = df.values.tolist()
 columns = df.columns.values.tolist()
 feature_list = columns[8:]
-x = [math.log(f, 2) for f in factors]
+
 for problem_id in range(1, 6):
-    plt.figure(figsize=(15, 20))
+    boxes = []
     for i in range(len(feature_list)):
-        pvalue = []
-        for f in factors:
-            ks_string = df[(df["problem_id"] == float(problem_id)) & 
-                (df["scale_factor"] == float(f))][feature_list[i]].tolist()[0]
-            ks_list = string_to_list(ks_string)
-            pvalue += [ks_list[1]]
-        plt.subplot(9, 6, i+1)
-        plt.ylim(-0.1, 1.1)
-        plt.plot(x, pvalue)
-        plt.xlabel("$\log_2{scale\_factor}$")
-        plt.axhline(0.05, color="red", linestyle=":")
-        plt.title(feature_list[i])
+        p_string = df[(df["problem_id"] == float(problem_id)) &
+                        (df["is_rotate"] == 0.0)][feature_list[i]].tolist()[0]
+        p = string_to_list(p_string)
+        p_mean = np.mean(p)
+        diff = []
+        for rotate_lim in range(30):
+            # parse distribution
+            q_string = df[(df["problem_id"] == float(problem_id)) &
+                          (df["rotate_lim"] == float(rotate_lim)) &
+                          (df["is_rotate"] == 1.0)][feature_list[i]].tolist()[0]
+            q = string_to_list(q_string)
+            q_mean = np.mean(q)
+            diff_ = min(abs((p_mean-q_mean)/p_mean), 10.)
+            diff += [diff_]
+        boxes += [diff]
+    plt.figure(figsize=(20, 5))
+    plt.boxplot(boxes, labels=feature_list)
+    plt.ylabel("%")
+    plt.xticks(rotation=90)
     plt.tight_layout()
-    plt.savefig("pvalue_scale_" + str(problem_id) + ".png")
-    plt.cla()
+    plt.savefig("{}_mean.png".format(problem_id))
